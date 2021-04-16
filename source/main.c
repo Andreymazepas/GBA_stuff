ddf7cf28b45f951311d3986c25c651e5d04583d9
Andrey Mazépas <andreymazepas@gmail.com>
2021-04-15 22:46:55 -0300

    the second first program

--- source/main.c
+++ source/main.c
@@ -1,56 +1,14 @@
-// #include "Intellisense.h"
+#include "toolbox.h"
 
+int main()
+{
+    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
 
-// int main()
-// {
-// 	//set GBA rendering context to MODE 3 Bitmap Rendering
-// 	*(unsigned int*)0x04000000 = 0x0403;
+    m3_plot( 120, 80, RGB15(31, 0, 0) );    // or CLR_RED
+    m3_plot( 136, 80, RGB15( 0,31, 0) );    // or CLR_LIME
+    m3_plot( 120, 96, RGB15( 0, 0,31) );    // or CLR_BLUE
 
-// 	int t = 0;
-// 	while(1){
-// 		int x,y;
-// 		for(x = 0; x < 240; ++x){
-// 			for( y = 0; y < 160; ++y){
-// 				((unsigned short*)0x06000000)[x+y*240] = ((((x&y)+t) & 0x1F) << 10)|
-// 				((((x&y)+t*3)&0x1F)<<5) | ((((x&y)+t * 5)&0x1F)<<0);
-// 			}
-// 		}
-// 		t=t+30;
-// 	}
-// 	return 0;
-// }
+    while(1);
 
-
-#include <gba_console.h>
-#include <gba_video.h>
-#include <gba_interrupt.h>
-#include <gba_systemcalls.h>
-#include <gba_input.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-//---------------------------------------------------------------------------------
-// Program entry point
-//---------------------------------------------------------------------------------
-int main(void) {
-//---------------------------------------------------------------------------------
-
-
-	// the vblank interrupt must be enabled for VBlankIntrWait() to work
-	// since the default dispatcher handles the bios flags no vblank handler
-	// is required
-	irqInit();
-	irqEnable(IRQ_VBLANK);
-
-	consoleDemoInit();
-
-	// ansi escape sequence to set print co-ordinates
-	// /x1b[line;columnH
-	iprintf("\x1b[10;10HHello World!\n");
-
-	while (1) {
-		VBlankIntrWait();
-	}
+    return 0;
 }
-
-
