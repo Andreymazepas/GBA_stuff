2f9cd37852b5b03da445bf8b8d6acee2f9a08452
Andrey Mazépas <andreymazepas@gmail.com>
2021-04-18 17:52:52 -0300

    tonc is nice

--- source/main.c
+++ source/main.c
@@ -2,11 +2,20 @@
 
 int main()
 {
-    REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+    int ii, jj;
 
-    m3_plot( 120, 80, RGB15(31, 0, 0) );    // or CLR_RED
-    m3_plot( 136, 80, RGB15( 0,31, 0) );    // or CLR_LIME
-    m3_plot( 120, 96, RGB15( 0, 0,31) );    // or CLR_BLUE
+	REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
+
+	m3_fill(RGB15(12, 12, 14));
+	for(ii=0; ii<=14; ii++)
+	{
+		jj= 2*ii+7;
+        m3_line(0, 11*ii, 17*ii, 160, RGB15(jj, 0, jj));
+        //m3_line(0, 160-11*ii, 17*ii, 0, RGB15(jj, 0, jj));
+        m3_line(17*ii, 0, 240, 11*ii, RGB15(jj, 0, jj));
+        //m3_line(240-17*ii, 160, 240, 11*ii, RGB15(jj, 0, jj));
+
+	}
 
     while(1);
 
